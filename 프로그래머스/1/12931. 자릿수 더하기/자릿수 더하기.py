4b585b45e8bd49e250321595a618cbf4e4ffbaae
Jeonghoo Son <80911999+jhson929@users.noreply.github.com>
2025-02-17 10:25:54 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,11 +1,4 @@
 def solution(n):
-    s = str(n)
-    li = list(s)
-    answer = 0
-    
-    length = len(li)
-    
-    for num in li:
-        answer += int(num)
+    answer = sum([int(num) for num in str(n)])
 
     return answer
