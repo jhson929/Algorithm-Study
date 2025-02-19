facea0d7a79e73ea61330385cdb130d97a63b5ff
Jeonghoo Son <80911999+jhson929@users.noreply.github.com>
2025-02-19 15:37:34 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,10 +1,3 @@
-def solution(n):
-    answer = ""
-    if n == 1:
-        return "수"
-    elif n % 2 == 0:
-        answer = "수박" * int((n ** 1/2))
-    else:
-        answer = "수박" * int((n ** 1/2)) + "수"
+def solution(n):    
+    return "수박" * (n//2) + "수" * (n%2)
     
-    return answer
