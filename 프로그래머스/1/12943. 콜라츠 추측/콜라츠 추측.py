f47a676f4beb9be15e2c4d40c2d4063fca77b7c7
Jeonghoo Son <80911999+jhson929@users.noreply.github.com>
2025-02-19 10:51:55 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.24 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,18 +1,14 @@
 def solution(num):
-    if num == 1:
-        return 0
+    answer = 0
     
-    count = 0
-    while True:
+    while num != 1:
         if num % 2 == 0:
             num /= 2
         else:
             num = 3 * num + 1
+        answer += 1
         
-        count += 1
-        if count == 500:
+        if answer == 500:
             return -1
-        elif num == 1:
-            break
-    
-    return count
+        
+    return answer
