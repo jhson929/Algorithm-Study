8842d72f9bd15780ebe10d8b1c1acebb853581ea
Jeonghoo Son <80911999+jhson929@users.noreply.github.com>
2025-02-20 16:07:41 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,17 +1,8 @@
 def solution(a, b):
-    answer = []
-    if a > b:
-        a, b = b, a # a < b임을 보장
-    
-    # 최대공약수
-    x = 1
-    for i in range(1, a+1):
-        if a % i == 0 and b % i == 0:
-            x = i
-    answer.append(x)
-    
-    # 최소공배수
-    y = x * (a // x) * (b // x)
-    answer.append(y)
-    
+    c, d = max(a, b), min(a, b)
+    t = 1
+    while t > 0: # t가(나머지가) 0이 될때까지
+        t = c % d
+        c, d = d, t
+    answer = [c, int(a*b/c)]
     return answer
